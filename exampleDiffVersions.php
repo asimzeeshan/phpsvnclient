46732b5f9228a888ba8ff12934753955b07d112c
forpdfsending <forpdfsending@gmail.com>
2011-08-05 05:34:28 +0000

    A small cleaning.

--- exampleDiffVersions.php
+++ exampleDiffVersions.php
@@ -6,12 +6,7 @@ ignore_user_abort(true);
 
 require_once("phpsvnclient.php");
 
-require_once 'Text/Diff.php';
-require_once 'Text/Diff/Renderer.php';
-require_once 'Text/Diff/Renderer/unified.php';
-
 $phpsvnclient = new phpsvnclient("http://ihtika.googlecode.com/svn/");
 
 $phpsvnclient->diffVersions("trunk/", "42", "55");
-//$phpsvnclient->diffVersions("trunk/", "0", "72");
 ?>
